25060ae1684b71dfb87bb3fc26f25588b87da514
Qusaix <qusaiznemat6@gmail.com>
2021-08-10 22:19:37 +0300

    T

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if($this->app->environment('local')) {
-            \URL::forceScheme('https');
-                    }
+        // if($this->app->environment('local')) {
+        //     \URL::forceScheme('https');
+        //             }
     }
 }
