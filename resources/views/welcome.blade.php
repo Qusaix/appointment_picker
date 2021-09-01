e81ba1cd79be82a619ea0a6b17cd54a7687ee440
Qusaix <qusaiznemat6@gmail.com>
2021-09-01 16:56:59 +0300

    optmize the speed of the website

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -361,21 +361,21 @@
   <div id="preloader"></div>
 
   <!-- Vendor JS Files -->
-  <script src='{{asset('fullcalendar/main.js')}}'></script>
-  <script src="{{asset('assets/vendor/jquery/jquery.min.js')}}"></script>
-  <script src="{{asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-  <script src="{{asset('assets/vendor/jquery.easing/jquery.easing.min.js')}}"></script>
-  <script src="{{asset('assets/vendor/php-email-form/validate.js')}}"></script>
-  <script src="{{asset('assets/vendor/waypoints/jquery.waypoints.min.js')}}"></script>
-  <script src="{{asset('assets/vendor/counterup/counterup.min.js')}}"></script>
-  <script src="{{asset('assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
-  <script src="{{asset('assets/vendor/venobox/venobox.min.js')}}"></script>
-  <script src="{{asset('assets/vendor/owl.carousel/owl.carousel.min.js')}}"></script>
-  <script src="{{asset('assets/vendor/typed.js/typed.min.js')}}"></script>
-  <script src="{{asset('assets/vendor/aos/aos.js')}}"></script>
+  <script defer src='{{asset('fullcalendar/main.js')}}'></script>
+  <script defer src="{{asset('assets/vendor/jquery/jquery.min.js')}}"></script>
+  <script defer src="{{asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script defer src="{{asset('assets/vendor/jquery.easing/jquery.easing.min.js')}}"></script>
+  <script defer src="{{asset('assets/vendor/php-email-form/validate.js')}}"></script>
+  <script defer src="{{asset('assets/vendor/waypoints/jquery.waypoints.min.js')}}"></script>
+  <script defer src="{{asset('assets/vendor/counterup/counterup.min.js')}}"></script>
+  <script defer src="{{asset('assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+  <script defer src="{{asset('assets/vendor/venobox/venobox.min.js')}}"></script>
+  <script defer src="{{asset('assets/vendor/owl.carousel/owl.carousel.min.js')}}"></script>
+  <script defer src="{{asset('assets/vendor/typed.js/typed.min.js')}}"></script>
+  <script defer src="{{asset('assets/vendor/aos/aos.js')}}"></script>
   <!-- Template Main JS File -->
-  <script src="{{asset('assets/js/main.js')}}"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+  <script defer src="{{asset('assets/js/main.js')}}"></script>
+  <script defer src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
 
   <!-- Calender -->
 </body>
