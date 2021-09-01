ad95074d4c2cf80a53f636e43763aef03a258eca
Qusaix <qusaiznemat6@gmail.com>
2021-09-01 16:40:34 +0300

    optmize the speed of the website

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
   <link href="{{asset('assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  {{-- <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet"> --}}
 
   <!-- Vendor CSS Files -->
   <link href="{{asset('assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
@@ -27,8 +27,6 @@
   <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
 
   <link href='{{asset('fullcalendar/main.css')}}' rel='stylesheet' />
-<script src='{{asset('fullcalendar/main.js')}}'></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
   
 
 <style>
@@ -363,6 +361,7 @@
   <div id="preloader"></div>
 
   <!-- Vendor JS Files -->
+  <script src='{{asset('fullcalendar/main.js')}}'></script>
   <script src="{{asset('assets/vendor/jquery/jquery.min.js')}}"></script>
   <script src="{{asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
   <script src="{{asset('assets/vendor/jquery.easing/jquery.easing.min.js')}}"></script>
@@ -376,6 +375,7 @@
   <script src="{{asset('assets/vendor/aos/aos.js')}}"></script>
   <!-- Template Main JS File -->
   <script src="{{asset('assets/js/main.js')}}"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
 
   <!-- Calender -->
 </body>
